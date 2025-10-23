2832c913b9023fe5c01260c20c545dd121b9e9cc
Steven Pham <stevenpham.dev@gmail.com>
2025-10-23 12:55:58 -0400

    feat(frontend): setup React Router with page routes

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import HistoryPage from './pages/HistoryPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<HistoryPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
